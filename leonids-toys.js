eca3ebd92d93ca20c5f2485e4ce438d13368a1f9
Glenn Dixon Jr <steve@stevenbrownlee.com>
2021-10-31 11:33:05 -0500

    first commit

---
+++ leonids-toys.js
@@ -0,0 +1,39 @@
+const connectFour = {
+    id: 1,
+    name: "Connect Four",
+    brand: "Hasbro Gaming",
+    inStock: true,
+    quantity: 7,
+    price: 10.99,
+    minAge: 6,
+    maxAge: 99,
+    players: 2,
+    category: "Strategy"
+}
+
+const uno = {
+    id: 2,
+    name: "UNO",
+    brand: "Mattel",
+    inStock: true,
+    quantity: 4,
+    price: 4.99,
+    minAge: 2,
+    maxAge: 10,
+    players: 7 + "+",
+    category: "Card"
+}
+
+const uno = {
+    id: 3,
+    name: "Monopoly",
+    brand: "Hansbro Gaming",
+    inStock: false,
+    quantity: 0,
+    price: 15.97,
+    minAge: 8,
+    maxAge: 99,
+    minPlayers: 2,
+    maxPlayers: 6,
+    category: "Board"
+}
